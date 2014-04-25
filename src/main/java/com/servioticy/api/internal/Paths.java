b461fb1c6395cdfaa145a90fb1f99837cdddd100
David Carrera <dcarrera@Davids-MacBook-Pro-2.local>
2014-04-25 23:28:37 +0200

    Fixed bug: need for getAllSubscriptions and getExternalSubscriptions

--- src/main/java/com/servioticy/api/internal/Paths.java
+++ src/main/java/com/servioticy/api/internal/Paths.java
@@ -53,7 +53,7 @@ public class Paths {
     if (so == null)
       throw new ServIoTWebApplicationException(Response.Status.NOT_FOUND, "The Service Object was not found.");
 
-    String response = so.responseSubscriptions(streamId);
+    String response = so.responseSubscriptions(streamId, false);
 
     // Generate response
     if (response == null)
